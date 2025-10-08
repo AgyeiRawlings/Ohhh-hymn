de63e76b85234282060cc7413c69155ae47d612d
Agyei <agyeirawlings35@gmail.com>
2025-10-08 17:40:10 +0000

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,12 @@
-
+@echo off
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+set DEFAULT_JVM_OPTS=
+if defined JAVA_HOME (
+    set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+) else (
+    set JAVA_EXE=java.exe
+)
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% -cp "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
