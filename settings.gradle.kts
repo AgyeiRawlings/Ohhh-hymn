ba355cb853d66a6c3cc4c8dc27eaf555a8e37191
Agyei <agyeirawlings35@gmail.com>
2025-10-17 02:32:18 +0000

    Change repository mode to FAIL_ON_PROJECT_REPOS

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
+cat > settings.gradle <<'EOF'
 pluginManagement {
     repositories {
         google()
@@ -5,14 +6,14 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
-
 rootProject.name = "Ohhh-hymn"
 include(":app")
+EOF
+
